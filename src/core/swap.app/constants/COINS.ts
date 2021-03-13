6640d82a05dae56915a39cc043a0eb6f592432bd
Jake <jfla15@student.aau.dk>
2021-03-14 00:17:42 +0100

    Update COINS.ts

--- src/core/swap.app/constants/COINS.ts
+++ src/core/swap.app/constants/COINS.ts
@@ -65,6 +65,13 @@ export const COIN_DATA = {
     model: COIN_MODEL.AB,
     precision: 18,
   },
+  'HPPY': {
+    ticker: 'HPPY',
+    name: 'HAPPY',
+    type: COIN_TYPE.ETH_TOKEN,
+    model: COIN_MODEL.AB,
+    precision: 18,
+  },
 }
 
 
